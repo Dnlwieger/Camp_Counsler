cbb201c664e370eea9116c21b1ccb4abb9b10576
Daniel Wieger <dnlwieger@gmail.com>
2018-03-05 14:14:09 -0500

    express server running, handlebars rendering

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.listen(3000, function() {
+    console.log('express test server running on port 3000')
+});
+
+
